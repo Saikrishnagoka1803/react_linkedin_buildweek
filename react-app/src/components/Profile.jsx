5af25bb76bc6da4978fcc20762ae7d75b529a52e
Bimal27 <bimalsah1996@gmail.com>
2021-09-06 12:33:00 +0200

    updated

---
+++ react-app/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import "../style.css"
+
+class Profile extends Component{
+    render(){
+        return(
+            <h1>Profile</h1>
+        )
+    }
+}
+export default Profile
