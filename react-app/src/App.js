f11a794a0d9bc21c03b012d38c57cae01c6c7725
Sai Krishna Goka <gskr1803@gmail.com>
2021-09-09 14:54:32 +0100

    commit with Home Top

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,12 +4,11 @@ import Education from './components/Education';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Interests from './components/Interests';
 import Dashboard from './components/Dashboard';
-<<<<<<< Updated upstream
-=======
+
+
 import Profiles from "./components/Profiles"; 
 import SecondComponent from './components/ProfilePage/SecondComponent';
 import HomeTop from './components/Profilepart1/HomeTop';
->>>>>>> Stashed changes
 
 function App() {
   return (
@@ -17,27 +16,13 @@ function App() {
       <Dashboard />
       <Education/>
       <Interests/>
-<<<<<<< Updated upstream
+      <HomeTop />
+      <SecondComponent/>
       
     </div>
   );
-}
-=======
-      <Profiles heading="People also viewed"/>
-      <Profiles heading="People you may know"/> */}
-<<<<<<< Updated upstream
-      <HomeTop />
-=======
-      
->>>>>>> Stashed changes
-      <SecondComponent/>
-      {/* <SecondComponent/>
-      <SecondComponent/>
-      <SecondComponent/> */}
-     </div>  
 
-   );
  }
->>>>>>> Stashed changes
+
 
 export default App;
