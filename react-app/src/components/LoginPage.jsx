b51964011e9346b30ca1365300c896d4917a71fb
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-09-10 10:37:59 +0100

    Update LoginPage
    Form validation

--- react-app/src/components/LoginPage.jsx
+++ react-app/src/components/LoginPage.jsx
@@ -1,20 +1,48 @@
 import React from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap'
 import { Form } from 'react-bootstrap'
+import { useState } from 'react';
 
 export default function LoginPage() {
+
+    const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
     return (
         <Container>
             <Row className="justify-content-md-center my-5">
-                <Col xs={6}>
+                <Col xs={4} className="border border-white rounded-0 shadow py-3">
                 <h1>Sign in</h1>
                 <p>Stay updated on your proffesional world</p>
-                <Form>
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
                      <Form.Group controlId="formGroupEmail">
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control 
+                            required 
+                            type="email" 
+                            placeholder="Enter email" 
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                Please enter a valid email address
+                            </Form.Control.Feedback>
                     </Form.Group>
                      <Form.Group controlId="formGroupPassword">
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control 
+                            required 
+                            type="password" 
+                            placeholder="Password" 
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                Password incorrect. Typos, am I right?
+                            </Form.Control.Feedback>
                      </Form.Group>
                 </Form>
                 <a href="">Forgot password?</a>
