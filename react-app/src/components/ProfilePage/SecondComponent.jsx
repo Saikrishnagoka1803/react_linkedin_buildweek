6212729a46ed25c095ee5ef8ce88edb101bd1162
Sai Krishna Goka <gskr1803@gmail.com>
2021-09-10 09:28:50 +0100

    news feed image upload done

--- react-app/src/components/ProfilePage/SecondComponent.jsx
+++ react-app/src/components/ProfilePage/SecondComponent.jsx
@@ -5,6 +5,7 @@ import '../Education.css'
 import Loader from '../Loader'
 import { useEffect, useState } from 'react'
 import Icons from './Icons'
+import SecCompModel from './SecCompModel'
 
 
 const SecondComponent = () => {
@@ -12,6 +13,7 @@ const SecondComponent = () => {
     const [profilepost, setprofilepost] = useState([])
     const [length, setlength] = useState(1218)
     const [isLoading, setisLoading] = useState(false)
+    const [secModel, setsecModel] = useState(false)
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -42,14 +44,14 @@ const SecondComponent = () => {
 
     return (
 
-
+         
         isLoading ? <Loader /> : profilepost && profilepost.slice(length - 15, length).reverse().map(ele => <Container className='mt-3' style={{ backgroundColor: 'white' }} >
 
             <Row className='justify-content-center' style={{ backgroundColor: 'white' }}>
                 <Col className='shadow' offset='2' style={{ borderTop: '1px solid gray', borderRadius: '8px', backgroundColor: 'white' }}>
 
                     <div className='d-flex justify-content-start mt-2 mb-2'>
-                        <img style={{ borderRadius: '50px', width: '52px', height: '52px' }} height='60' src="https://images.unsplash.com/photo-1630754328109-013e18676561?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="..." roundedCircle />
+                        <img style={{ borderRadius: '50px', width: '52px', height: '52px' }} height='60' src={ele.image} alt="..." roundedCircle />
 
                         <div>
                             <div className="d-flex ">
@@ -73,11 +75,14 @@ const SecondComponent = () => {
                 </Col>
             </Row>
             <Row className='justify-content-center'>
+                <SecCompModel id={ele._id} image={ele.image}/>
+                {/* <Col style={{ padding: '0px' }}>
 
-                <Col style={{ padding: '0px' }}>
-
-                    <img className='img-fluid' style={{ objectFit: 'cover', overflow: 'hidden' }} src="https://images.unsplash.com/photo-1628191079308-6183b016b3df?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="" />
-                </Col>
+                    <img 
+                    onClick={(e) => {setsecModel(true)}}
+                    className='img-fluid' style={{ objectFit: 'cover', overflow: 'hidden' }} 
+                    src="https://images.unsplash.com/photo-1628191079308-6183b016b3df?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="" />
+                </Col> */}
             </Row>
             <Icons />
         </Container>
