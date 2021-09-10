af43b064016aee47949b2d3cac4d605c79e8fc34
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-09-10 10:44:13 +0100

    Update
    Form validation fixed

--- react-app/src/components/LoginPage.jsx
+++ react-app/src/components/LoginPage.jsx
@@ -21,34 +21,34 @@ export default function LoginPage() {
         <Container>
             <Row className="justify-content-md-center my-5">
                 <Col xs={4} className="border border-white rounded-0 shadow py-3">
-                <h1>Sign in</h1>
-                <p>Stay updated on your proffesional world</p>
-                <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                     <Form.Group controlId="formGroupEmail">
-                            <Form.Control 
-                            required 
-                            type="email" 
-                            placeholder="Enter email" 
-                            />
-                            <Form.Control.Feedback type="invalid">
-                                Please enter a valid email address
-                            </Form.Control.Feedback>
-                    </Form.Group>
-                     <Form.Group controlId="formGroupPassword">
-                            <Form.Control 
-                            required 
-                            type="password" 
-                            placeholder="Password" 
-                            />
-                            <Form.Control.Feedback type="invalid">
-                                Password incorrect. Typos, am I right?
-                            </Form.Control.Feedback>
-                     </Form.Group>
-                </Form>
-                <a href="">Forgot password?</a>
-                <Button variant="primary" size="lg" block>
-                    Sign in
-                </Button>
+                        <h1>Sign in</h1>
+                        <p>Stay updated on your proffesional world</p>
+                        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                            <Form.Group controlId="formGroupEmail">
+                                    <Form.Control 
+                                    required 
+                                    type="email" 
+                                    placeholder="Enter email" 
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        Please enter a valid email address
+                                    </Form.Control.Feedback>
+                            </Form.Group>
+                            <Form.Group controlId="formGroupPassword">
+                                    <Form.Control 
+                                    required 
+                                    type="password" 
+                                    placeholder="Password" 
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        Password incorrect. Typos, am I right?
+                                    </Form.Control.Feedback>
+                            </Form.Group>
+                        <a href="">Forgot password?</a>
+                        <Button type="submit" variant="primary" size="lg" block>
+                            Sign in
+                        </Button>
+                        </Form>
                 </Col>
             </Row>
         </Container>
